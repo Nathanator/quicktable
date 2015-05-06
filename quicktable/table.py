f41fb0d5353d3bc746cefe7a7b31d880f56a84b7
tomgodkin <thgodkin@gmail.com>
2015-05-06 02:03:02 +0100

    Better __str__

--- quicktable/table.py
+++ quicktable/table.py
@@ -10,14 +10,29 @@ class Table:
         self.row_tuple = namedtuple('Row', self.columns.keys())
 
     def append(self, *values):
+        """Append the values as a new row to the table."""
         for value, column in zip(values, self.columns.values()):
             column.values.append(value)
 
+    def filter(self, function):
+        return filter(function, self)
+
+    @staticmethod
+    def _map_unpack(function, iterable):
+        """Unpack each element of iterable into map."""
+        return map(lambda element: function(*element), iterable)
+
     def __iter__(self):
-        return iter(map(lambda row: self.row_tuple(*row), zip(*self.columns.values())))
+        return iter(self._map_unpack(self.row_tuple, zip(*self.columns.values())))
 
     def __getitem__(self, index):
         if isinstance(index, slice):
             return list(islice(self, index.start, index.stop, index.step))
 
         return self.row_tuple(*[column[index] for column in self.columns.values()])
+
+    def __str__(self):
+        return '<quicktable.Table object at {0}>'.format(hex(id(self)))
+
+    def __repr__(self):
+        return str(self)
